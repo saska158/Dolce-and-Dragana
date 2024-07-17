280e66f0bda51ad8651e4706b075ed1d1f19cfde
Saska <mikic.saska@gmail.com>
2024-07-17 11:40:24 +0200

    Password Recover Added

--- src/PasswordRecover.js
+++ src/PasswordRecover.js
@@ -1,17 +1,70 @@
+import { useState } from "react"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+
 export default function PasswordRecover() {
+    const auth = getAuth()
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('We will send you an email with instructions on how to recover it.')
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const sendResetEmail = async (e, email) => {
+        e.preventDefault()
+        setLoading(true)
+        setError(null)
+        try {
+            await sendPasswordResetEmail(auth, email)
+            console.log("Password reset email sent!")
+            setMessage("Password reset email sent!")
+            setEmail('')
+        } catch(error) {
+            let customMessage
+            if (error.code === 'auth/invalid-email') {
+                customMessage = `WARNING: Enter a valid e-mail address.`
+            } else {
+                customMessage = `Error signing up: ${error.message}`
+            }
+            console.error("Error sending password reset email:", error)
+            setError(customMessage)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    if(loading) {
+        return (
+            <div className="content-container">
+              <h1>Loading...</h1>
+            </div>
+        )
+    }
+
+    if(error) {
+        return (
+            <div className="content-container">
+              <div className="pop-up-box">
+                <p>{error}</p>
+                <button onClick={() => setError(null)}>CLOSE</button>
+              </div>
+            </div>
+        )
+    }
+
     return (
         <div className="content-container">
             <div className="form-container">
                 <div>
                     <h4>RESET PASSWORD</h4>
-                    <p style={{fontSize: '.7rem'}}>We will send you an email with instructions on how to recover it</p>
+                    <p>{message}</p>
                     <form className="form">
                         <input
                           type="email"
                           placeholder="E-MAIL" 
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
                           required
                         />
-                        <button>CONTINUE</button>
+                        <button onClick={e => sendResetEmail(e, email)}>CONTINUE</button>
                     </form>
                 </div>
             </div>
