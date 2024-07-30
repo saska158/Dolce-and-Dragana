eae0920f19d7f86522e0f8967ed74bbdbe7936c3
Saska <mikic.saska@gmail.com>
2024-07-30 16:28:19 +0200

    update

--- src/Homepage.js
+++ src/Homepage.js
@@ -37,7 +37,7 @@ const Homepage = () => {
     }
 
     const handleDrag = (data) => {
-      const { x, deltaX } = data
+      const { x } = data
       gsap.to(videoRefs.current[index].current, {
         duration: 0.3,
         x: x
